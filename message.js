af55f69133b9bc86a9dea75ae8c00d46651cbf68
bonbons0220 <bsouter@jhu.edu>
2017-04-10 13:24:33 -0400

    works for pages with body, not for pages using framesets

--- message.js
+++ message.js
@@ -1,33 +1,32 @@
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = "//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js";
-script.onreadystatechange = docLoaded;
-script.onload = docLoaded;
+/*
+	This script adds a top SciServer Banner to the top of the page.
+	Content depends on the context as determined from the "/dr##/" in the 
+	document url. 
+*/
 
-var head = document.getElementsByTagName('head')[0];
-head.appendChild(script);
-
-function docLoaded(){
-	// LOAD STYLESHEET
-	//$('head').append('<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">');
-	$('head').append('<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">');
+// PARSE URL FOR DR# TO DETERMINE WHICH MESSAGE TO SHOW. 
+// DON''T SHOW MESSAGE ON TOOLS PAGES - THEY USE FRAMESETS.
+if ( !( /\/explore\//i.test( document.URL ) || 
+		/\/chart\//i.test( document.URL ) || 
+		/\/quicklook\//i.test( document.URL ) ) && 
+	 ( /dr([0-9]+)/i.test( document.URL ) ) ) {
+	 
+	var drurl = /\/dr([0-9]+)\//i.exec( document.URL )[1];
+	var script = document.createElement('script');
+	var head = document.getElementsByTagName('head')[0];
+	script.type = 'text/javascript';
 	
-	// LOAD MESSAGE.TXT
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			$('body').prepend( this.responseText ); 
-			//var target = $('a[href*="DR"]' , 'td.midbodyText')[0].text;
-			var drmsg = $('a[href*="DR"]' , 'td.midbodyText')[0].text;
-			if ( /[0-9]+/.exec(drmsg) ) {
-				$( 'span#drnumber' ).text( /[0-9]+/.exec(drmsg) );
-			}else {
-				$( 'span#drnumber' ).text( "1" );
-			}
-			
-
-		}
-	};
-	xhttp.open("GET", "//www.sciserver.org/messages/messagedr1to7.txt", true);
-	xhttp.send();
+	switch(drurl) {
+		case "1":
+		case "2":
+		case "3":
+		case "4":
+		case "5":
+		case "6":
+		case "7":
+			script.src = "http://www.sciserver.org/messages/messagedr1to7.js";
+			head.appendChild(script);
+			break;
+		default:
+	}
 }
--- messagedr1to7.js
+++ messagedr1to7.js
@@ -1,47 +1,63 @@
-// TEST THIS ON ANY PAGE. OPEN THE CONSOLE WITH F12. 
-// USE JQUERIFY (google it) TO ADD JQUERY TO THE PAGE.
-// THEN TYPE THE FOLLOWING:
-// $.getScript("http://www.sciserver.org/messages/messagedr1to7.js"></script>');
+/* 
+ INSERTS A HEADER AT THE TOP OF THE PAGE 
+ WITH A LINK TO THE LATEST DATA RELEASE
+ AS DETERMINED FROM THE URL.
+ TO TEST THIS ON ANY PAGE OPEN THE CONSOLE BY
+ PRESSING  F12 THEN ENTER THE FOLLOWING:
+ 
+script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = "http://www.sciserver.org/messages/messagedr1to7.js";
+head = document.getElementsByTagName('head')[0];
+head.appendChild(script);
 
+   OR
+   
+RUN jQuerify (google it) AND ENTER IN THE CONSOLE:
+
+$.getScript("http://www.sciserver.org/messages/messagedr1to7.js") 
+
+*/
 var script = document.createElement('script');
+var head = document.getElementsByTagName('head')[0];
 script.type = 'text/javascript';
 script.src = "//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js";
 script.onreadystatechange = docLoaded;
 script.onload = docLoaded;
-
-var head = document.getElementsByTagName('head')[0];
 head.appendChild(script);
 
 function docLoaded(){
 	// LOAD STYLESHEET
-	//$('head').append('<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">');
-	
-	$('head').append('<link rel="stylesheet" type="text/css" href="//www.sciserver.org/messages/messagedr1to7.css">');
+	$('head').append('<link rel="stylesheet" type="text/css" href="//www.sciserver.org/messages/message.css">');
 	
 	// LOAD MESSAGE.TXT
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			
-			$('body').prepend( this.responseText ); 
+			// INJEXT THE HTML IF THE BODY EXISTS 
+			// (N.B. THERE IS NO 'BODY' TAG IN THE EXPLORE, CHART, OR QUICKLOOK)
+			if ( $('body').length ) {
 			
-			// FIND AND SHOW THE DATA RELEASE #
-			//var drmsg = $('a[href*="Welcome"]' , 'td.midbodyText')[0].text;
-			var drmsg = $("td.midbodyText:contains('Welcome')")[0].text;
-			if ( /[0-9]+/.exec(drmsg) ) {
-				$( 'span#drnumber' ).text( /[0-9]+/.exec(drmsg) );
-			}else {
-				$( 'span#drnumber' ).text( "1" );
+				$('body').prepend( this.responseText ); 
+			
+				// FIND AND SHOW THE DATA RELEASE #
+				var drurl = /\/dr([0-9]+)\//i.exec( document.URL )[1];
+				$( 'span#drnumber' ).text( drurl );
+							
+				// MOVE ANY ABSOLUTE POSITIONED ELEMENTS DOWN THE HEIGHT AND TOP + BOTTOM MARGINS
+				var y = $($('div#header-message')[0]).outerHeight();
+				var el = $('*').filter(function(){
+				   return ( $(this).css('position') === 'absolute' ) || ( $(this).css('POSITION') === 'absolute' );
+				});
+				$(el).each (function () {
+					$(this).css( {top: parseInt( $(this).css('top') ) + y + 'px' } );
+				});
+				
+			} else if ( $('frameset').length ) {
+				newframeset='<frameset><frame src=""//www.sciserver.org/messages/messagedr1to7.html""></frame></frameset>'
+				$($('frameset *:first-child')[0]).before(newframeset);				
 			}
-			
-			// MOVE ANY ABSOLUTE POSITIONED ELEMENTS DOWN THE HEIGHT AND TOP + BOTTOM MARGINS
-			var y = $($('div#header-message')[0]).outerHeight();
-			var el = $('*').filter(function(){
-			   return ( $(this).css('position') === 'absolute' ) || ( $(this).css('POSITION') === 'absolute' );
-			});
-			$(el).each (function () {
-				$(this).css( {top: parseInt( $(this).css('top') ) + y + 'px' } );
-			});
 		}
 	};
 	xhttp.open("GET", "//www.sciserver.org/messages/messagedr1to7.txt", true);
