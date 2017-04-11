a360347085a10788ec3f4eb62799c71951d45fc3
bonbons0220 <bsouter@jhu.edu>
2017-04-11 14:52:24 -0400

    added messages for non dr1-11 pages with login to sciserver and link to support. added sciserver logo to all messages

--- message.js
+++ message.js
@@ -1,32 +1,111 @@
 /*
-	This script adds a top SciServer Banner to the top of the page.
-	Content depends on the context as determined from the "/dr##/" in the 
+	Adds a top SciServer Banner to the top of a page.
+	Banner content depends on context as determined from the 
 	document url. 
 */
 
-// PARSE URL FOR DR# TO DETERMINE WHICH MESSAGE TO SHOW. 
-// DON''T SHOW MESSAGE ON TOOLS PAGES - THEY USE FRAMESETS.
-if ( !( /\/explore\//i.test( document.URL ) || 
-		/\/chart\//i.test( document.URL ) || 
-		/\/quicklook\//i.test( document.URL ) ) && 
-	 ( /dr([0-9]+)/i.test( document.URL ) ) ) {
-	 
-	var drurl = /\/dr([0-9]+)\//i.exec( document.URL )[1];
-	var script = document.createElement('script');
-	var head = document.getElementsByTagName('head')[0];
-	script.type = 'text/javascript';
+/*
+   Test this on any page. Open the console with F12
+   then do one of the following:
+   
+   Copy and past the following to the console:
+   
+script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = "http://www.sciserver.org/messages/message.js";
+head = document.getElementsByTagName('head')[0];
+head.appendChild(script);
+
+   or
+   
+   RUN jQuerify (google it) AND ENTER IN THE CONSOLE:
+   
+$.getScript("http://www.sciserver.org/messages/message.js") 
+   
+*/
+debug=false;
+if (debug) console.log('drurl ' + drurl);
+
+// Load jQuery. Makes everything easier...
+var script = document.createElement('script');
+var head = document.getElementsByTagName('head')[0];
+script.type = 'text/javascript';
+script.src = "//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js";
+script.onreadystatechange = jqLoaded;
+script.onload = jqLoaded;
+head.appendChild(script);
 	
-	switch(drurl) {
-		case "1":
-		case "2":
-		case "3":
-		case "4":
-		case "5":
-		case "6":
-		case "7":
-			script.src = "http://www.sciserver.org/messages/messagedr1to7.js";
-			head.appendChild(script);
-			break;
-		default:
+function jqLoaded(){
+	
+	// Get Data Release #
+	drurl=0;
+	if ( /\/dr[0-9]+\//i.test( document.URL ) ) drurl = /\/dr([0-9]+)\//i.exec( document.URL )[1];
+	
+	if (drurl>=1 && drurl<=7) {
+		message = 'messagedr1to7';
+	} else if (drurl>=8 && drurl <=11 ) {
+		message = 'messagedr8to11';
+	} else {
+		message = 'message';
 	}
+		
+	// does this have a body,
+	if ( $('body').length ) {
+	
+		// stylesheet
+		$('head').append('<link rel="stylesheet" type="text/css" href="//www.sciserver.org/messages/message.css">');
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				$('body').prepend( this.responseText );
+				postprocess(drurl);
+			}
+		}
+		xhttp.open("GET", "//www.sciserver.org/messages/" + message + ".txt", true);
+		xhttp.send();
+	
+	// or framesets,
+	} else if ( $('frameset').length ) {
+		
+		msgframe=window.document.createElement('frame');
+		msgframe.name='msgframe';
+		msgframe.id='msgframe';
+		msgframe.src='//www.sciserver.org/messages/' + message + '.html';
+		
+		fset0=$('frameset')[0];
+		fset1=fset0.cloneNode(true);
+		$(fset0).children().each( function() { fset0.removeChild(this) } );
+		
+		fset0.insertBefore(fset1,fset0.firstChild);
+		fset0.insertBefore(msgframe,fset0.firstChild);
+		fset0.rows="50,*";
+		fset0.cols="*";
+		
+		postprocess( drurl);
+	
+	// or something else.
+	} else {
+		return;
+	}
+	
+	return;
 }
+function postprocess(){
+	// SWAP LOGIN/LOGOUT IF NEEDED
+	if ( /token=/i.test( document.URL ) ) $('#header-login-link').prop('href' , $('#header-login-link').prop('href') + document.URL)
+	
+	// FIND AND SHOW THE DATA RELEASE #
+	if (drurl) $( 'span#drnumber' ).text( 'This is SDSS Data Release ' + drurl + '.' );
+				
+	// MOVE ANY ABSOLUTE POSITIONED ELEMENTS DOWN THE HEIGHT AND TOP + BOTTOM MARGINS
+	var y = $($('div#header-message')[0]).outerHeight();
+	var el = $('*').filter(function(){
+	   return ( $(this).css('position') === 'absolute' ) || ( $(this).css('POSITION') === 'absolute' );
+	});
+	$(el).each (function () {
+		$(this).css( {top: parseInt( $(this).css('top') ) + y + 'px' } );
+	});
+}
+ function keystone_login() {
+	 window.location.href = "http://portal.sciserver.org/login-portal/Account/Login?callbackUrl=http://skyserver.sdss.org%2fdr13%2fen%2fhome.aspx";
+ }
